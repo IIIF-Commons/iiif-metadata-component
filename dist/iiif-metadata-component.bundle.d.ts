0dbea8471334c86249648e3663f8e1658b552d93
edsilv <e.silverton@gmail.com>
2016-10-26 15:56:01 +0100

    update manifold

--- dist/iiif-metadata-component.bundle.d.ts
+++ dist/iiif-metadata-component.bundle.d.ts
@@ -66,7 +66,7 @@ interface JQuery {
     verticalMargins(): number;
     verticalPadding(): number;
 }
-// manifesto.js v0.2.4 https://github.com/viewdir/manifesto
+// manifesto.js v1.0.0 https://github.com/viewdir/manifesto
 declare module exjs {
     var version: string;
 }
